76eea3f4c567247358764803e6901ad6655a4964
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-11 18:24:42 +0000

    Components and Elements

--- App.js
+++ App.js
@@ -4,17 +4,38 @@ import ReactDOM from "react-dom/client";
 // JSX (Transpiled before it reaches the JS using Babel in Parcel)
 // JSX => Reaact.createElement => ReactElement-JS Oject => HTMLElement(render)
 
-const jsxHeading = <h1 id = "heading">React using JSX</h1>;
+const el = <span>Rect Element</span>
+
+const title = (
+<h1 id = "head" tabIndex="5">
+    {el}
+    This is Rect Element 
+</h1>
+);
+const Title1 = () => (
+<h1 id = "head" tabIndex="5">
+    This is React Function Component
+    {/* <HeadingComponent - Don't do this it will creaate aan infinit loop */}
+</h1>
+);
 
 //Both React Core and JSX make Ojects 
 
 //React Fuctional Components (Its Name always needs to be in Captial Letters)
-const HeadingComponent = () => {
-    return <h1>React Functional Components</h1>
 
-}
-const HeadingComponent2 = () => (
-    <h1>React Functional Components</h1>
+const HeadingComponent = () => (
+    <div id="container">
+    {title}
+    
+    {/* This 3 things are same:
+    -> {Title()}
+    -> <Title/>
+    -> <Title></Title>
+    */}
+
+    <Title1 />
+    <h1 id="heading">React Functional Components</h1>
+    </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
