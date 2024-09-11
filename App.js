67edc1b1cb75706c5fc145b970e4067398ac40dc
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-11 18:54:07 +0000

    First Project

--- App.js
+++ App.js
@@ -1,43 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX (Transpiled before it reaches the JS using Babel in Parcel)
-// JSX => Reaact.createElement => ReactElement-JS Oject => HTMLElement(render)
-
-const el = <span>Rect Element</span>
-
-const title = (
-<h1 id = "head" tabIndex="5">
-    {el}
-    This is Rect Element 
-</h1>
+const Header = () => (
+    <div className="header">
+        <div className="logo-container">
+            <img className="logo" src="https://static.vecteezy.com/system/resources/previews/025/722/194/original/food-basket-icon-logo-isolated-on-white-background-vector.jpg" />
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+                <li>Login</li>
+            </ul>
+        </div>
+    </div>
 );
-const Title1 = () => (
-<h1 id = "head" tabIndex="5">
-    This is React Function Component
-    {/* <HeadingComponent - Don't do this it will creaate aan infinit loop */}
-</h1>
-);
 
-//Both React Core and JSX make Ojects 
-
-//React Fuctional Components (Its Name always needs to be in Captial Letters)
-
-const HeadingComponent = () => (
-    <div id="container">
-    {title}
-    
-    {/* This 3 things are same:
-    -> {Title()}
-    -> <Title/>
-    -> <Title></Title>
-    */}
-
-    <Title1 />
-    <h1 id="heading">React Functional Components</h1>
+const AppLyout = () => (
+    <div className="app">
+        <Header/>
     </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLyout/>);
