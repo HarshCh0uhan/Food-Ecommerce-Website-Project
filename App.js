175ff2b1873940e73996038961de5e0f6fc8c061
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-11 16:35:41 +0000

    package.json

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ const parent = React.createElement("div", {id: "parent"}, [
         React.createElement("h1", {}, "I'm an h1 tag"),
         React.createElement("h2", {}, "I'm an h2 tag")
     ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")
+    ]),
 ]);
 
 console.log(parent);
