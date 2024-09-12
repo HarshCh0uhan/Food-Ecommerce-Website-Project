ebff718b25e0dc723ff02945e013047c7ebc71cf
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-12 11:49:38 +0000

    Made Navbar

--- App.js
+++ App.js
@@ -1,27 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {RiRestaurantLine} from "@remixicon/react";
 
 const Header = () => {
-    return ( <div className="header">
-        <div className="logo-container">
-            <img className="logo" src="https://static.vecteezy.com/system/resources/previews/025/722/194/original/food-basket-icon-logo-isolated-on-white-background-vector.jpg" />
-        </div>
+    return ( 
+        <nav className="header">
+        <RiRestaurantLine
+            size={36} // set custom `width` and `height`
+            color="black" // set `fill` color
+            className="logo" // add custom class name
+        />
         <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-                <li>Login</li>
-            </ul>
+            <div>Home</div>
+          <div>Menu</div>
+          <div>Cart</div>
+          <div>Log in</div>
         </div>
-    </div>
+      </nav>
     );
+};
+
+const ResCard = () => {
+    return(
+        <div className="res-card">
+
+        </div>
+    )
+}
+
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="res-container">
+                {/* //ResCard */}
+            </div>
+        </div>
+    )
 };
 
 const AppLyout = () => {
     return <div className="app">
         <Header/>
+        <Body/>
     </div>
 };
 
