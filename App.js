68c11ac5ae11c49480b9b57860f7be71138b39b1
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-12 18:24:50 +0000

    NavBar Done

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {RiRestaurantLine, RiArrowDownSLine, RiHomeLine, RiAccountCircleFill, RiSearchLine, RiShoppingCart2Line} from "@remixicon/react";
 
-
-
 const Header = () => {
     return ( 
         <nav className="header">
@@ -32,11 +30,23 @@ const Header = () => {
     );
 };
 
+const ResCard = () => {
+    return (
+        <div className="res-card">
+            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/7/cab23c6f-50b5-4fb1-900b-ff683a5806b1_857176.jpg" />
+            <h3>Meghna Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h3>4.4 Stars</h3>
+            <h3>38 Minutes</h3>
+        </div>
+    )
+}
+
 const Body = () => {
     return(
         <div className="body">
             <div className="res-container">
-                {/* //ResCard */}
+                <ResCard/>
             </div>
         </div>
     )
