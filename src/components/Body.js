6ab6377db878e434520a6069a081aabd03096227
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-13 17:41:18 +0000

    Filtering aand useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,26 @@
 import ResCard from "./ResCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 
 const Body = () => {
+    // Local State Variable
+
+    const[listOfRes, setListOfRes] = useState([]);
+
     return(
         <div className="body">
             <div className="filter">
-                <button className="filter-btn">Top Rated Restaurants</button>
+                <button className="filter-btn" onClick={() => {
+                    const filteredList = resList.filter(
+                        (res) => res.info.avgRating > 4.5
+                    );
+                    setListOfRes(filteredList);
+                }}>Top Rated Restaurants</button>
                 <hr className="line"></hr>
             </div>
             <div className="res-container">
-                {resList.map(items => <ResCard key={items.info.id} resData={items}/>) }
+                {listOfRes.map(items => <ResCard key={items.info.id} resData={items}/>) }
             </div>
         </div>
     )
