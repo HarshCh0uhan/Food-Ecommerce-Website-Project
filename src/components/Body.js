b764cacf7fa4897c65d8cf6722acba12f13c87b5
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-13 17:04:38 +0000

    Menu Buttos

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,6 +4,10 @@ import resList from "../utils/mockData";
 const Body = () => {
     return(
         <div className="body">
+            <div className="filter">
+                <button className="filter-btn">Top Rated Restaurants</button>
+                <hr className="line"></hr>
+            </div>
             <div className="res-container">
                 {resList.map(items => <ResCard key={items.info.id} resData={items}/>) }
             </div>
