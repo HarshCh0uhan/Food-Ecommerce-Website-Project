7bb72150ac588e0230506079a2513cd423954438
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-14 18:29:26 +0000

    Shimmer UI Done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,39 +1,46 @@
+import { RiLoaderLine } from "@remixicon/react";
+// import resList from "../utils/mockData";
 import ResCard from "./ResCard";
-import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 import { useEffect, useState } from "react";
 
 const Body = () => {
     // Local State Variable
 
-    const[listOfRes, setListOfRes] = useState(resList);
+    const[listOfRes, setListOfRes] = useState([]);
 
     useEffect(() => {
         fetchData();
     }, []);
 
+    
     const fetchData = async () =>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5623169&lng=77.2803832&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         
-        setListOfRes(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setListOfRes(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    
+    if(listOfRes.length == 0){
+        return <Shimmer/>;
     }
 
     return(
         <div className="body">
             <div className="filter">
                 <button className="close-btn" onClick={() => {
-                    setListOfRes(resList);
+                    setListOfRes();
                 }}>X</button>
                 <button className="filter-btn" onClick={() => {
-                    const topRatedRes = resList.filter(
+                    const topRatedRes = listOfRes.filter(
                         (res) => res.info.avgRating > 4.5
                     );
                     setListOfRes(topRatedRes);
                     
                 }}>Top Rated Restaurants</button>
                 <button className="filter-btn" onClick={() => {
-                    const lowDelTime = resList.filter(
-                        (res) => res.info.sla.deliveryTime < 50
+                    const lowDelTime = listOfRes.filter(
+                        (res) => res.info.sla.deliveryTime < 30
                     );
                     setListOfRes(lowDelTime);
                 }}>Low Delivery Time</button>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,36 @@
+const Shimmer = () => {
+    return ( 
+        <div className="shimmer-body">
+            <div className="shimmer-filter">
+                <button className="shimmer-close-btn"></button>
+                <button className="shimmer-filter-btn"></button>
+                <button className="shimmer-filter-btn"></button>
+            </div>
+            <hr className="shimmer-line"></hr>
+            <div className="shimmer-container">
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+                <div className="shimmer-res-card"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Shimmer;
