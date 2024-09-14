144e4e70b4c9ba905a3b07f0f98b2f8b71e01900
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-14 10:13:41 +0000

    Filter Improved

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,6 +12,9 @@ const Body = () => {
     return(
         <div className="body">
             <div className="filter">
+                <button className="close-btn" onClick={() => {
+                    setListOfRes(resList);
+                }}>X</button>
                 <button className="filter-btn" onClick={() => {
                     const topRatedRes = resList.filter(
                         (res) => res.info.avgRating > 4.5
@@ -20,17 +23,14 @@ const Body = () => {
 
                     
                 }}>Top Rated Restaurants</button>
-                <button className="close-btn" onClick={() => {
-                    setListOfRes(resList);
-                }}><RiCloseCircleLine/></button>
                 <button className="filter-btn" onClick={() => {
                     const lowDelTime = resList.filter(
                         (res) => res.info.sla.deliveryTime < 50
                     );
                     setListOfRes(lowDelTime);
                 }}>Low Delivery Time</button>
-                <hr className="line"></hr>
             </div>
+            <hr className="line"></hr>
             <div className="res-container">
                 {listOfRes.map(items => <ResCard key={items.info.id} resData={items}/>) }
             </div>
