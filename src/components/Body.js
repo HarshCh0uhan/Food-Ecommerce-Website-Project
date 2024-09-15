9f595c23489de3d2fc19cede896672c7b616aa70
Harsh_Chouhan <harshcoding2004@gmail.com>
2024-09-15 00:57:47 +0000

    Filter Btn Done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,4 @@
-import { RiLoaderLine } from "@remixicon/react";
-// import resList from "../utils/mockData";
+import resList from "../utils/mockData";
 import ResCard from "./ResCard";
 import Shimmer from "./Shimmer";
 import { useEffect, useState } from "react";
@@ -14,34 +13,48 @@ const Body = () => {
     }, []);
 
     
-    const fetchData = async () =>{
+    const fetchData = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5623169&lng=77.2803832&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         
         setListOfRes(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    }
+    };
+
+    const [topRes, setTopRes] = useState("Top Rated Restaurants");
+
+    const [lowTime, setLowTime] = useState("Low Delivery Time");
     
 //  Conditional Rendering or Shimmer UI
 
-    return (listOfRes.length == 0) ? <Shimmer/> : (
+    return (listOfRes.length === 0) ? <Shimmer/> : (
         <div className="body">
             <div className="filter">
-                <button className="close-btn" onClick={() => {
-                    setListOfRes();
-                }}>X</button>
                 <button className="filter-btn" onClick={() => {
                     const topRatedRes = listOfRes.filter(
                         (res) => res.info.avgRating > 4.5
                     );
-                    setListOfRes(topRatedRes);
+                    if(topRes == "Top Rated Restaurants"){
+                        setTopRes("X      Top Rated Restaurant");
+                        setListOfRes(topRatedRes);
+                    }else {
+                        setTopRes("Top Rated Restaurants");
+                        fetchData();
+                    }            
                     
-                }}>Top Rated Restaurants</button>
+                }}>{topRes}</button>
                 <button className="filter-btn" onClick={() => {
                     const lowDelTime = listOfRes.filter(
-                        (res) => res.info.sla.deliveryTime < 30
+                        (res) => res.info.sla.deliveryTime < 40
                     );
-                    setListOfRes(lowDelTime);
-                }}>Low Delivery Time</button>
+
+                    if(lowTime == "Low Delivery Time"){
+                        setLowTime("X Low Delivery Time");
+                        setListOfRes(lowDelTime);
+                    }else {
+                        setLowTime("Low Delivery Time");
+                        fetchData();
+                    }
+                }}>{lowTime}</button>
             </div>
             <hr className="line"></hr>
             <div className="res-container">
